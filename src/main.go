2a38a1d832327858b6386ac9572fdb98ef34ccda
ysa23 <ysa.angel@gmail.com>
2017-07-15 21:16:00 +0300

    Add phase one of parser - first step of parsing let statements

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! I'll bet your wondering why the red suit\n", user.Name)
+	fmt.Printf("Lets get the ball rolling - you can now start typing commands:\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ src/parser/parser_test.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"ast"
+	"lexer"
+	"testing"
+)
+
+func TestLetStatments(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("Expected ParseProgram to return an object, and not nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("Expected program.Statements to contain 3 statements, but got %d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("Expected token literal to be 'let' but was %q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("Expected let statement but got %T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("Expected Name value identifer to be '%s' but got '%s'", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("Expected Name to be '%s' but got '%s'", name, letStmt.Name)
+		return false
+	}
+
+	return true
+}
